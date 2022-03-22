961422c7b6495c6a0046d2b5a77fa7b0caa895a6
Malavika-a <98263801+Malavika-a@users.noreply.github.com>
2022-03-22 12:59:46 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,16 @@
+public class Operation{
+	public static void main(String args[]){
+		int a=10;
+		int b=10;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a/b;
+		
+		System.out.println("Addition a+b:"+c);
+		System.out.println("Subtraction a-b:"+d);
+		System.out.println("Multiplication a*b:"+e);
+		System.out.println("Division a/b:"+f);
+		
+	}
+}
